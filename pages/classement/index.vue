b1fa88d0d49e539f4ac97eb24f6437bb5c90df4a
Guillaume <guillaume.chebib@hotmail.fr>
2020-08-02 01:18:55 +0200

    Add color in table based on rank of the team

--- pages/classement/index.vue
+++ pages/classement/index.vue
@@ -4,7 +4,7 @@
     <div>
       <ul class="classement">
       <template v-for='team in classement.api.standings'>
-          <b-table responsive="true" striped hover :items="team" :fields="field" :tbody-tr-class="rowClass" >
+          <b-table responsive="true" striped hover :items="team" :fields="field" >
             <template v-slot:cell(logo)="data">
               <b-avatar  size="sm" square variant="light" v-bind:src="data.item.logo"></b-avatar>
             </template>
@@ -20,6 +20,18 @@
   export default {
     async asyncData ({ $http }) {
       const data = await $http.$get('/api/classement')
+
+      for(let i = 0; i < data.api.standings.length; i++) {
+
+        let array=data.api.standings[i].length
+
+        for (let j = 0; j < array; j++) {
+          let obj = data.api.standings[i][j];
+          if (obj.rank === 1) {
+            data.api.standings[i][j]._cellVariants = {rank: "OrangeCustom"};
+          }
+        }
+      }
       return {
         field:["rank","logo","teamName","forme","goalsDiff","points"],
         classement: data
@@ -31,10 +43,10 @@
       }
     },
     methods: {
-      rowClass(item, type) {
-        if (!item || type !== 'row') return
-        if (item.rank === 1 ) return 'table-OrangeCustom'
-      }
+
+    },
+    computed:{
+
     }
   }
 </script>
